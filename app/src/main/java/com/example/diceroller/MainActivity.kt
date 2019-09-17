de411de56067917c90bfa68b295a7b9a57314cf9
Yusley <5003489915@student.csn.edu>
2019-09-17 13:39:44 -0700

    Added rollDice method event to event listener, generate random number integer 1-6

--- app/src/main/java/com/example/diceroller/MainActivity.kt
+++ app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -3,7 +3,10 @@ package com.example.diceroller
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.TextView
 import android.widget.Toast
+import kotlin.random.Random
+
 
 class MainActivity : AppCompatActivity() {
 
@@ -12,6 +15,14 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         val rollButton: Button =  findViewById(R.id.roll_button)
         rollButton.text = "Let's Roll"
-        rollButton.setOnClickListener { Toast.makeText(this, "button clicked", Toast.LENGTH_SHORT).show() }
+        rollButton.setOnClickListener {  //Toast.makeText(this, "button clicked", Toast.LENGTH_SHORT).show()
+            rollDice()
+        }
+    }
+
+    private fun rollDice() {
+        val resultText: TextView = findViewById(R.id.results_text)
+        val randomInt = java.util.Random().nextInt(6) + 1
+        resultText.text = randomInt.toString()
     }
 }
